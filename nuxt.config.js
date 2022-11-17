8aaaff35fab00ea7a19d241d4fc695cbca8108a6
Angrymouse <rykov.nikas@gmail.com>
2022-11-17 14:16:11 +0200

    remove deps

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,4 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-import vitePluginRequire from "vite-plugin-require";
 export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss'],
     ssr: false,
